629244d5e011cee8e9f9463f7af9b6db0dc67d8f
Sidi Gaber <mohamdeesayed@outlook.com>
2022-08-23 11:28:10 +0200

    set mail settings

---
+++ resources/views/mail/register.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h1>Welcome Home</h1>
+</div>
+<div>
+    <p>you have registered your account succfully.</p>
+</div>
+
+<p>click<a href="{{ route('Home') }}">here</a> to visit our page</p>
